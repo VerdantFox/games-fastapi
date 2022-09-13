bf2e27fa7d2d38258ae20653f1067a40666ae832
Teddy Williams <theodore.f.williams@gmail.com>
2022-09-13 11:59:37 -0600

    change utils/deploy.sh defaults

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -43,14 +43,14 @@ log() {
 # ---------------------------------------------------------------------------
 # SCRIPT START
 # ---------------------------------------------------------------------------
-log INFO "Running utils/deploy.sh with vars: FROM_SCRATCH=${FROM_SCRATCH:-}, IF_NEEDED=${IF_NEEDED:-}, DEVELOPMENT=${DEVELOPMENT:-}"
+log INFO "Running utils/deploy.sh with vars: FROM_SCRATCH=${FROM_SCRATCH:-}, IF_NEEDED=${IF_NEEDED:-}, PROD=${PROD:-}"
 
 # Set up log for crontab jobs (including deployment log)
 mkdir -p "$(pwd)/logs"
 
-# Default to deploying the "release-branch" branch
-# (can override to current branch with DEVELOPMENT=1)
-if [ "${DEVELOPMENT:-}" != "1" ]
+# By default, don't checkout updated 'release-branch' before deploying
+# (if PROD=1, then we do checkout the latest release-branch)
+if [ "${PROD:-}" == "1" ]
 then
     log INFO "Rebasing latest release-branch..."
     git fetch origin --prune
@@ -64,9 +64,14 @@ then
 fi
 
 
-# Default to installing the below crontabs
-# (can override to not install with DEVELOPMENT=1)
-if [ "${DEVELOPMENT:-}" != "1" ]
+# By default, don't install the crontab that runs cron jobs
+# (if PROD=1, then we do install.)
+# Can use `crontab -l` to check if the crontab exists and `crontab -r` to remove the crontab.
+# Crontab has 3 jobs:
+# - Every 5 minutes run this deploy script, only if the 'release-branch' has changed
+# - 1x per day run this deploy script to rebuild the docker images from scratch (to get latest security patches)
+# - 1x per day delete > 1 week old logs from the 'logs/' directory
+if [ "${PROD:-}" == "1" ]
 then
 touch "$CRON_LOG"
 log INFO "Installing new crontab..."
@@ -74,9 +79,9 @@ crontab << ENDCRON
 # Games-FastAPI scheduled tasks
 # This crontab was generated automatically and should not be edited.
 # Deploy (from scratch): every day at 2:07AM.
-7 2 * * * PATH=$PATH TZ=America/Denver FROM_SCRATCH=1 "$(pwd)/utils/deploy.sh" >> "$CRON_LOG"  2>&1
+7 2 * * * PATH=$PATH TZ=America/Denver FROM_SCRATCH=1 PROD=1 "$(pwd)/utils/deploy.sh" >> "$CRON_LOG"  2>&1
 # Deploy (only if needed): every 5 minutes.
-*/5 * * * * PATH=$PATH TZ=America/Denver IF_NEEDED=1 "$(pwd)/utils/deploy.sh" >> "$CRON_LOG"  2>&1
+*/5 * * * * PATH=$PATH TZ=America/Denver IF_NEEDED=1 PROD=1 "$(pwd)/utils/deploy.sh" >> "$CRON_LOG"  2>&1
 # Remove week-old logs: every day at 3:07AM.
 7 3 * * * find "$(pwd)/logs/" -mtime +7  -exec rm {} \; >> "$CRON_LOG"  2>&1
 ENDCRON
